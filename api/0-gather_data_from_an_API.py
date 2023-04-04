f35f20f27451a19e9ce4888d787c2e64a57003d2
juan celu <juan celu>
2023-04-04 13:49:52 -0300

    doc

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress. """
+"""script"""
 
 
 from urllib import request
