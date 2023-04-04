6bc443b15903cfbd27c9036f376bf5e1064466c2
juan_latitude <fernan2lopezkto@gmail.com>
2023-04-04 11:19:01 -0300

    change to try personal test

--- 0-gather_data_from_an_API.py
+++ 0-gather_data_from_an_API.py
@@ -12,12 +12,14 @@ employee_ID = sys.argv[1]
 url = "https://jsonplaceholder.typicode.com/todos/{}".format(employee_ID)
 
 # make request and save in 'html' var
-with urllib.request.urlopen(url) as response:
+with request.urlopen(url) as response:
    html = response.read()
 
 # format output to example:
 # Employee Ervin Howell is done with tasks(8/20):
 
-EMPLOYEE_NAME = url[name] # name of the employee
-NUMBER_OF_DONE_TASKS = # number of completed tasks
-TOTAL_NUMBER_OF_TASKS = # total number of tasks
+#EMPLOYEE_NAME = url[name] # name of the employee
+#NUMBER_OF_DONE_TASKS = # number of completed tasks
+#TOTAL_NUMBER_OF_TASKS = # total number of tasks
+
+print(html)
