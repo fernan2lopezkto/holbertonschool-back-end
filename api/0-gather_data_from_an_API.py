7fb689291b5f010cf1c756ca0c356182c27bb9f2
juan celu <juan celu>
2023-04-05 13:54:55 -0300

    start with help

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,49 +1,15 @@
-#!/usr/bin/python3
-"""script"""
+#!/usr/bin/python3 
+""" Request a Rest API""" 
+import json 
+import requests 
+from sys import argv
 
-
-import json
-import sys
-from urllib import request
-
-if __name__ == "__main__":
-    # var
-    total_task = 0
-    task_ok = 0
-    employee_ID = int(sys.argv[1])
-    url = "https://jsonplaceholder.typicode.com/todos"
-    url_user = "https://jsonplaceholder.typicode.com/users"
-
-    resp = requests.get(url_user).json()
-
-    '''
-    # recuest for users
-    response = request.urlopen(url_user)
-    users = response.read().decode("utf-8")
-    list = json.loads(users)
-
-    # recuest for task
-    response_task = request.urlopen(url)
-    tasks = response_task.read().decode("utf-8")
-    list_task = json.loads(tasks)
-
-
-    # found name of user by id
-    for i in list:
-        if i["id"] == employee_ID:
-            emp_name = i["name"]
-
-    # work with task
-    for task in list_task:
-        if task['userId'] == employee_ID:
-            total_task = total_task + 1
-            if task["completed"] == True:
-                task_ok = task_ok + 1
-
-    '''            
-    # EMPLOYEE_NAME: name of the employee
-
-    # output first line
-    l1output = "Employee {} is done with tasks({}/{}):".format()
-
-    print(l1output)
+if __name__ == '__main__':
+    '''code'''
+    emp_id = int(argv[1])
+    todo_url = ''
+    users_url = ''
+    
+    users = requests.get(users_url)
+    tasks = requests.get(todo_url)
+  
