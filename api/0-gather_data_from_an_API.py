c4b8bb19e2f6f807e3893a4beecdde52a03f1a10
juan celu <juan celu>
2023-04-04 23:49:25 -0300

    mmm

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,3 +8,8 @@ if __name__ == "__main__":
     employee_ID = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/todos"
     url_user = "https://jsonplaceholder.typicode.com/users"
+
+    reaponse = request.urlopen(url_user)
+        for i in request:
+            if i[id] = employee_ID:
+                print(i[name])
