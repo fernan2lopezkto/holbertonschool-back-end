5554535ab45c05fcda351f34679f3f263accd0d5
juan celu <juan celu>
2023-04-04 22:22:44 -0300

    delete coments

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,18 +8,3 @@ if __namr__ == "__main__":
   employee_ID = sys.argv[1]
   url = "https://jsonplaceholder.typicode.com/todos"
   url_user = "https://jsonplaceholder.typicode.com/users"
-
-"""
-  # make request and save in 'html' var
-  with request.urlopen(url) as response:
-   html = response.read()
-
-# format output to example:
-# Employee Ervin Howell is done with tasks(8/20):
-
-#EMPLOYEE_NAME = url[name] # name of the employee
-#NUMBER_OF_DONE_TASKS = # number of completed tasks
-#TOTAL_NUMBER_OF_TASKS = # total number of tasks
-
-print(html)
-"""
