d0a9c4c3af8345c657d88a85255caf388a0dfb96
juan celu <juan celu>
2023-04-04 22:50:05 -0300

    gh

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,9 +5,6 @@ import sys
 from urllib import request
 
 if __name__ == "__main__":
-  pass
-  """documentary
-  employee_ID = sys.argv[1]
-  url = "https://jsonplaceholder.typicode.com/todos"
-  url_user = "https://jsonplaceholder.typicode.com/users"
-  """
+    employee_ID = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/todos"
+    url_user = "https://jsonplaceholder.typicode.com/users"
