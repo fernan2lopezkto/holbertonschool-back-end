242498916dec80ae1ab494be0ea2cc5cb07ccf89
juan_latitude <fernan2lopezkto@gmail.com>
2023-04-05 10:35:03 -0300

    kkk

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -14,6 +14,9 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/todos"
     url_user = "https://jsonplaceholder.typicode.com/users"
 
+    resp = requests.get(url_user).json()
+
+    '''
     # recuest for users
     response = request.urlopen(url_user)
     users = response.read().decode("utf-8")
@@ -37,7 +40,10 @@ if __name__ == "__main__":
             if task["completed"] == True:
                 task_ok = task_ok + 1
 
+    '''            
+    # EMPLOYEE_NAME: name of the employee
+
     # output first line
-    l1output = "Employee {} is done with tasks({}/{}):".format(emp_name, task_ok, total_task)
+    l1output = "Employee {} is done with tasks({}/{}):".format()
 
     print(l1output)
