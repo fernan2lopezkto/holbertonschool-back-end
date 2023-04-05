93fdb5065c11de2bbead960e7116f707f754bc9f
juan_latitude <fernan2lopezkto@gmail.com>
2023-04-05 03:01:33 -0300

    can read and see the dict users

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python3
 """script"""
 
+
+import json
 import sys
 from urllib import request
 
 if __name__ == "__main__":
-    employee_ID = sys.argv[1]
+    employee_ID = int(sys.argv[1])
     url = "https://jsonplaceholder.typicode.com/todos"
     url_user = "https://jsonplaceholder.typicode.com/users"
-    reaponse = request.urlopen(url_user)
-        for i in request:
-            if i[id] = employee_ID:
-                print(i[name])
-  
+
+    response = request.urlopen(url_user)
+    
+    users = response.read().decode("utf-8")
+
+    list = json.loads(users)
+
+    for i in list:
+        if i["id"] == employee_ID:
+            emp_name = i["name"]
+
+    l1output = "Employee {} is done with tasks(8/20):".format(emp_name)
+
+    print(l1output)
