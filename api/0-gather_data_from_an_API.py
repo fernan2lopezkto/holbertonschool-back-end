ad145f03eda037ac5ab349035507b68ea7b24bd1
juan celu <juan celu>
2023-04-05 21:47:01 -0300

    complete with help

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,15 +1,39 @@
 #!/usr/bin/python3 
-""" Request a Rest API""" 
-import json 
-import requests 
+""" Request a Rest API"""
+
+
+import json
+import requests
 from sys import argv
+
 
 if __name__ == '__main__':
     '''code'''
     emp_id = int(argv[1])
-    todo_url = ''
-    users_url = ''
-    
+    emp_name = ''
+    todo_url = 'https://jsonplaceholder.typicode.com/todos'
+    users_url = 'https://jsonplaceholder.typicode.com/users'
+
     users = requests.get(users_url)
     tasks = requests.get(todo_url)
-  
+
+    for user in users.json():
+        if user['id'] == emp_id:
+            emp_name = user['name']
+            break
+
+    task_ok = 0
+    total_task = 0
+    task_title =[]
+
+    for tsk in tasks.json():
+        if tsk['userId'] == emp_id:
+            total_task += 1
+            if tak['completed']:
+                task_ok += 1
+                task_title.append(tak['title'])
+
+    print('Employee {} is done with tasks({}/{}):'.format(emp_name, task_ok, total_task))
+
+    for title in task_title:
+        print('\t {}'.format(title))
