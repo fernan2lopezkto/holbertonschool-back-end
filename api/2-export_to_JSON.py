ee2e02d4545f7c614e946af59b71a6ed9f52bd96
juan_latitude <fernan2lopezkto@gmail.com>
2023-04-07 17:51:51 -0300

    fix any things

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" Request a Rest API"""
+""" 
+script to export data in the JSON format
+"""
 
 import json
 import requests
@@ -23,19 +25,14 @@ if __name__ == '__main__':
             eN = user['name']
             us_name = user['username']
             break
-
-    """
-    for tsk in tasks.json():
-        if tsk['userId'] == emp_id:
-            tTL += 1
-            if tsk['completed']:
-                tOK += 1
-                task_title.append(tsk['title'])
-    """
                 
     # Format must be: 
-    # { "USER_ID": [{"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"},
-    #               {"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"},
+    # { "USER_ID": [{"task": "TASK_TITLE",
+    #               "completed": TASK_COMPLETED_STATUS,
+    #               "username": "USERNAME"},
+    #               {"task": "TASK_TITLE",
+    #               "completed": TASK_COMPLETED_STATUS,
+    #               "username": "USERNAME"},
     #                   ... ]}
 
     data = {}
@@ -50,15 +47,3 @@ if __name__ == '__main__':
 
     with open(f_name, 'w') as file:
         json.dump(data, file)
-
-'''
-    with open(f_name, 'w') as f:
-        writer = csv.writer(f, delimiter=',',
-                            quotechar='"', quoting=csv.QUOTE_ALL)
-        for tsk in tasks.json():
-            if tsk["userId"] == emp_id:
-                t_Ok = tsk["completed"]
-                t_Ttle = tsk["title"]
-                writer.writerow([emp_id, us_name, t_Ok, t_Ttle])
-
-'''
