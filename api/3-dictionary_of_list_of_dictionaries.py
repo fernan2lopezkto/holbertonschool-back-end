e0efcaada1414b49e12cb14e20f63cf0d30eba5f
juan_latitude <fernan2lopezkto@gmail.com>
2023-04-07 20:44:25 -0300

    mod

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" 
-script to export data in the JSON format
-"""
+""" script to export data in the JSON format """
+
 
 import json
 import requests
+
 
 if __name__ == '__main__':
     '''code'''
